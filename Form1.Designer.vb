d34a8b2dd45a0d29c92a448007a40d35e6c250ad
CSAR1018026 <CSAR1018026@lab.cict>
2024-11-15 12:29:49 +0800

    pogi jade

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,28 +23,88 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         ButtonTest = New Button()
+        Login = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        NameUser = New TextBox()
+        WordPass = New TextBox()
         SuspendLayout()
         ' 
         ' ButtonTest
         ' 
-        ButtonTest.Location = New Point(290, 188)
+        ButtonTest.Location = New Point(12, 12)
         ButtonTest.Name = "ButtonTest"
-        ButtonTest.Size = New Size(194, 75)
+        ButtonTest.Size = New Size(153, 48)
         ButtonTest.TabIndex = 0
-        ButtonTest.Text = "MYSQL TEST"
+        ButtonTest.Text = "Connection Test"
         ButtonTest.UseVisualStyleBackColor = True
+        ' 
+        ' Login
+        ' 
+        Login.Location = New Point(308, 296)
+        Login.Name = "Login"
+        Login.Size = New Size(167, 65)
+        Login.TabIndex = 1
+        Login.Text = "Login"
+        Login.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(53, 119)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(213, 47)
+        Label1.TabIndex = 2
+        Label1.Text = "Username: "
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(53, 182)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(213, 47)
+        Label2.TabIndex = 3
+        Label2.Text = "Password: "
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' NameUser
+        ' 
+        NameUser.Location = New Point(261, 139)
+        NameUser.Name = "NameUser"
+        NameUser.Size = New Size(267, 23)
+        NameUser.TabIndex = 4
+        ' 
+        ' WordPass
+        ' 
+        WordPass.Location = New Point(261, 202)
+        WordPass.Name = "WordPass"
+        WordPass.PasswordChar = "*"c
+        WordPass.Size = New Size(267, 23)
+        WordPass.TabIndex = 5
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(555, 405)
+        Controls.Add(WordPass)
+        Controls.Add(NameUser)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Login)
         Controls.Add(ButtonTest)
         Name = "Form1"
         Text = "MYSQL DATABASE Connection"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents ButtonTest As Button
+    Friend WithEvents Login As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents NameUser As TextBox
+    Friend WithEvents WordPass As TextBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -6,7 +6,7 @@ Public Class Form1
 
     Private Sub ButtonTest_Click(sender As Object, e As EventArgs) Handles ButtonTest.Click
         conn = New MySqlConnection
-        conn.ConnectionString = "server=localhost;userid=root;password='';database=test"
+        conn.ConnectionString = "server=127.0.0.1;userid=root;password='';database=test"
 
         Try
             conn.Open()
@@ -17,4 +17,36 @@ Public Class Form1
             conn.Close()
         End Try
     End Sub
+
+    Private Sub Login_Click(sender As Object, e As EventArgs) Handles Login.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=127.0.0.1;userid=root;password='';database=test"
+        Dim READER As MySqlDataReader
+
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "select * from test.edata WHERE user_name = '" & NameUser.Text & "' and password = '" & WordPass.Text & "'"
+            COMMAND = New MySqlCommand(Query, conn)
+            READER = COMMAND.ExecuteReader
+            Dim COUNT As Integer
+            COUNT = 0
+            While READER.Read
+                COUNT = COUNT + 1
+            End While
+
+            If COUNT = 1 Then
+                MessageBox.Show("UserName and password are correct")
+            ElseIf COUNT > 1 Then
+                MessageBox.Show("UserName and password are Duplicate")
+            Else
+                MessageBox.Show("UserName and password are not correct")
+            End If
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
 End Class
